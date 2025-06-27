8f17fe3dd9faffe9e2f1d2761196ef6506dae554
aarxv-999 <aarxv999@gmail.com>
2025-06-28 00:24:01 +0530

    Update manual_test.py

--- manual_test.py
+++ manual_test.py
@@ -7,22 +7,22 @@ def test_servo():
     print("Enter servo channel (0-5):")
     channel = int(input())
     if channel < 0 or channel > 5:
-        print("Invalid channel!")
+        print("wrong channel being used")
         return
-    print("Is it angle-based (a) or continuous (c)?")
+    print("enter 'a' if it is angle based or else 'c' if it is continuous")
     servo_type = input().lower()
     if servo_type == 'a':
-        print("Enter angle (0-180):")
+        print("enter angle (0-180)")
         angle = int(input())
         controller.set_angle(channel, angle)
-        print(f"Set channel {channel} to {angle} degrees")
+        print(f"set channel {channel} to {angle} degrees")
     elif servo_type == 'c':
-        print("Enter direction (clockwise, counterclockwise, stop):")
+        print("enter direction (clockwise, counterclockwise, stop)")
         direction = input().lower()
         controller.rotate_servo(channel, direction)
         print(f"Set channel {channel} to {direction}")
     else:
-        print("Invalid servo type!")
+        print("invalid servo type")
 
 while True:
     test_servo()
